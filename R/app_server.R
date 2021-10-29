ba2707b21ffec7f25a38e5a1d6db6c3bcb9092d6
Javier Gracia-Tabuenca <javier.graciatabuenca@tuni.fi>
2021-10-29 11:40:19 +0300

    reacivity working

--- R/app_server.R
+++ R/app_server.R
@@ -1,10 +1,75 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
-  # Your application server logic 
-  
+app_server <- function(input, output, session) {
+  # Your application server logic
+
+  # INFO connection tab ---------------------------------------------
+  r <- reactiveValues(cdm_webapi_conn = configCDMTools())
+
+  output$info_connstatus_dg <- toastui::renderDatagrid({
+    r$cdm_webapi_conn$conn_status_tibble  %>%
+      select(error, step, message) %>%
+      mutate(step = str_replace(step, "Test c", "C")) %>%
+      toastui::datagrid(
+        sortable = FALSE,
+        colnames = c("Status", "Connection", "Error Message"),
+        colwidths = c(100,350,NULL)
+      )%>%
+      toastui::grid_format(
+        "error", function(value) {
+          lapply(
+            X = value,
+            FUN = function(x) {
+              if (x)
+                fluidPage(tags$i(class = "fa fa-times-circle", style = "color: rgb(166,0,90)"),shiny::icon("fa-times-circle"))
+              else
+                fluidPage(tags$i(class = "fa fa-check-circle", style = "color: rgb(0,166,90)"),shiny::icon("fa-check-circle"))
+            }
+          )
+        }
+      )
+  })
+
+  shiny::observeEvent(input$info_connstatus_refresh_b,{
+    r$cdm_webapi_conn <- configCDMTools()
+  })
+
+
+  # IMPORT tab ---------------------------------------------
+  r2 <- reactiveValues(
+    cohortData = FinnGenTableTypes::empty_cohortData()
+    )
+
+
+  rf <-  mod_import_cohort_file_server("in_modal_import_cohorts")
+
+  output$importcohorts_cohorts_dg <- toastui::renderDatagrid({
+   rf() %>%
+    #r2$cohortData %>%
+      FinnGenTableTypes::summarise_cohortData() %>%
+      FinnGenTableTypes::table_summarycohortData(display_mode = "duration" )
+  })
+
+
+
+
+
+  # importcohorts_import_b modal
+  observeEvent(input$importcohorts_import_b, {
+    showModal( modal_import_cohorts() )
+  })
+
+
+
+  # INFO modules ---------------------------------------------
+  #mod_info_box_server("includedConceptsInfo", "test", "info_test.md")
+
+
+
 }
+
+
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -8,35 +8,103 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic 
-    fluidPage(
-      h1("CohortOperationsShinyApp")
+
+    # Your application UI logic
+    shinydashboard::dashboardPage(
+
+      # TITLE
+      shinydashboard::dashboardHeader(title = "Cohort Operations"),
+
+      ## SIDEBAR
+      shinydashboard::dashboardSidebar(
+        shinydashboard::sidebarMenu(
+
+          # mod_info_box_ui(
+          #   id = "includedConceptsInfo",
+          #   item = shinydashboard::menuItem(text = "test", tabName = "test", icon = icon("info"))
+          # ),
+
+          shinydashboard::menuItem("Info", tabName = "info", icon = icon("info")),
+          h4("Settings"),
+          shinydashboard::menuItem("Import Cohorts", tabName = "importcohorts", icon = icon("address-card-o")),
+          shinydashboard::menuItem("Operate Cohorts", tabName = "operatecohorts", icon = icon("sliders"))
+        )
+      ),
+
+      ## BODY
+      shinydashboard::dashboardBody(
+        shinydashboard::tabItems(
+          ## info
+          shinydashboard::tabItem(
+            tabName = "info",
+            includeMarkdown(app_sys("app/info_md/info_main_page.md")),
+            h6("Connection status:"),
+            toastui::datagridOutput("info_connstatus_dg"),
+            shiny::actionButton("info_connstatus_refresh_b", "Reconnect")
+          ),
+          ## Import Cohorts
+          shinydashboard::tabItem(tabName = "importcohorts",
+                  toastui::datagridOutput(outputId = "importcohorts_cohorts_dg"),
+                  #
+                  shiny::actionButton("importcohorts_import_b", "Import"), # calls modal_import_cohorts
+                  shiny::actionButton("importcohorts_delete_b", "Delete"),
+                  shiny::actionButton("importcohorts_save_b", "Save Cohorts"),
+          )
+        )
+      )
     )
   )
 }
 
+
+# MODAL called by importcohorts_import_b
+modal_import_cohorts <- function(){modalDialog(
+  size = "l",
+  title = "Import cohorts",
+  footer = modalButton("Cancel"),
+  easyClose = FALSE,
+  #
+  tabsetPanel(
+    type = "tabs",
+    # panel FILE
+    tabPanel("from File",
+             h2("This is possible if file in cohortTable format"),
+             mod_import_cohort_file_ui("in_modal_import_cohorts")
+    ),
+    # panel ATLAS
+    tabPanel("from Atlas",
+             selectInput('importcohorts_import_atlas_si', 'Select CDM-database:',
+                         c("DF8", "DF7", "DF6")),
+             #DT::renderDataTable(mock_atlas_cohorts),
+             actionButton("importcohorts_import_atlas_import_b", "Import Selected")
+    ),
+    # panel ENDPOINT
+    tabPanel("from Endploint",
+             h2("Possible if endpoint results in a BQ database in cohortTable format ")
+    )
+  )
+)}
+
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
-golem_add_external_resources <- function(){
-  
+golem_add_external_resources <- function() {
   add_resource_path(
-    'www', app_sys('app/www')
+    "www", app_sys("app/www")
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'CohortOperationsShinyApp'
+      path = app_sys("app/www"),
+      app_title = "CohortOperationsShinyApp"
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
-
