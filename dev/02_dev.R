2fbe7e37408628b1c6976fd4c98b9f2351bdb10f
Javier Gracia-Tabuenca <javier.graciatabuenca@tuni.fi>
2021-12-07 13:07:17 +0200

    ss

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -36,6 +36,7 @@ usethis::use_package("shinyjs")
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module( name = "info_box" )
+golem::add_module( name = "button_append_cohort" )
 #golem::add_module( name = "name_of_module2" ) # Name of the module
 golem::add_module( name = "connection_to_db" )
 #
